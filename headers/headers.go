9a12009edee90cea6345572f136997b33d90f41b
greycr0w <alexbant@teiser.gr>
2022-05-19 23:14:28 +0200

    Introduce Header struct, flattenr.Headers map and sort for cleaner output

--- headers/headers.go
+++ headers/headers.go
@@ -1,15 +1,41 @@
 package headers
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"sort"
+)
+
+type Header struct {
+	Name   string
+	Values []string
+}
 
 func ParseHeaders(headers http.Header) string {
 	var output string
+	var _headers []Header
+
+	//flatten r.Headers map to Header struct slice
 	for key, value := range headers {
-		output += key + ": "
-		for _, item := range value {
-			output += item
+		newHeader := Header{
+			Name:   key,
+			Values: value,
+		}
+		_headers = append(_headers, newHeader)
+	}
+
+	//sort Header slice by Name
+	sort.Slice(_headers, func(i, j int) bool {
+		return _headers[i].Name < _headers[j].Name
+	})
+
+	for _, value := range _headers {
+		output += fmt.Sprintf("%s: ", value.Name)
+		for _, field := range value.Values {
+			output += fmt.Sprintf("%s ", field)
 		}
 		output += "\n"
 	}
+
 	return output
 }
